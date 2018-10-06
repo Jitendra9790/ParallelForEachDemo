9587ce9ed78147ef21cf2be7d90a342059d15363
Jitendra9790 <43837472+Jitendra9790@users.noreply.github.com>
2018-10-06 12:49:08 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ParallelForEachDemo
+{
+    class Program
+    {
+        static List<int> digits = new List<int>();
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                digits.Add(i);
+            }
+
+            Stopwatch sw = Stopwatch.StartNew();
+            SequentialForEach();
+            sw.Stop();
+            Console.WriteLine("Time Taken By Sequential For Each Loop : " + sw.ElapsedMilliseconds);
+
+            sw = Stopwatch.StartNew();
+            ParallelForEach();
+            sw.Stop();
+            Console.WriteLine("Time Taken By Parallel For Each Loop : " + sw.ElapsedMilliseconds);
+
+            Console.ReadKey();
+        }
+
+        public static void SequentialForEach()
+        {
+            foreach (int i in digits)
+            {
+                Console.WriteLine("Sequential For Each : " + i);
+            }
+        }
+
+        public static void ParallelForEach()
+        {
+            Parallel.ForEach(digits, i => 
+            { 
+                Console.WriteLine("Parallel For Each : " + i); 
+            });
+        }
+    }
+}
